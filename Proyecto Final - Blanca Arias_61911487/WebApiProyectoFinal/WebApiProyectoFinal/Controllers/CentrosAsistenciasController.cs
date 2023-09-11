8692f9cf7336ba7679d463663228b9a475518b8e
BLANCAA99 <bariasmunoz913@unitec.edu>
2023-09-11 00:51:24 -0500

    Avance

---
+++ Proyecto Final - Blanca Arias_61911487/WebApiProyectoFinal/WebApiProyectoFinal/Controllers/CentrosAsistenciasController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiProyectoFinal;
+
+namespace WebApiProyectoFinal.Controllers
+{
+    public class CentrosAsistenciasController : ApiController
+    {
+        private Proyecto_FinalEntities db = new Proyecto_FinalEntities();
+
+        // GET: api/CentrosAsistencias
+        public IQueryable<CentrosAsistencia> GetCentrosAsistencias()
+        {
+            return db.CentrosAsistencias;
+        }
+
+        // GET: api/CentrosAsistencias/5
+        [ResponseType(typeof(CentrosAsistencia))]
+        public IHttpActionResult GetCentrosAsistencia(int id)
+        {
+            CentrosAsistencia centrosAsistencia = db.CentrosAsistencias.Find(id);
+            if (centrosAsistencia == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(centrosAsistencia);
+        }
+
+        // PUT: api/CentrosAsistencias/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCentrosAsistencia(int id, CentrosAsistencia centrosAsistencia)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != centrosAsistencia.Id_Asistencia)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(centrosAsistencia).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CentrosAsistenciaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CentrosAsistencias
+        [ResponseType(typeof(CentrosAsistencia))]
+        public IHttpActionResult PostCentrosAsistencia(CentrosAsistencia centrosAsistencia)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CentrosAsistencias.Add(centrosAsistencia);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CentrosAsistenciaExists(centrosAsistencia.Id_Asistencia))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = centrosAsistencia.Id_Asistencia }, centrosAsistencia);
+        }
+
+        // DELETE: api/CentrosAsistencias/5
+        [ResponseType(typeof(CentrosAsistencia))]
+        public IHttpActionResult DeleteCentrosAsistencia(int id)
+        {
+            CentrosAsistencia centrosAsistencia = db.CentrosAsistencias.Find(id);
+            if (centrosAsistencia == null)
+            {
+                return NotFound();
+            }
+
+            db.CentrosAsistencias.Remove(centrosAsistencia);
+            db.SaveChanges();
+
+            return Ok(centrosAsistencia);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CentrosAsistenciaExists(int id)
+        {
+            return db.CentrosAsistencias.Count(e => e.Id_Asistencia == id) > 0;
+        }
+    }
+}
---
+++ Proyecto Final - Blanca Arias_61911487/WebApiProyectoFinal/WebApiProyectoFinal/Controllers/CiudadanoesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApiProyectoFinal;
+
+namespace WebApiProyectoFinal.Controllers
+{
+    public class CiudadanoesController : ApiController
+    {
+        private Proyecto_FinalEntities db = new Proyecto_FinalEntities();
+
+        // GET: api/Ciudadanoes
+        public IQueryable<Ciudadano> GetCiudadanos()
+        {
+            return db.Ciudadanos;
+        }
+
+        // GET: api/Ciudadanoes/5
+        [ResponseType(typeof(Ciudadano))]
+        public IHttpActionResult GetCiudadano(int id)
+        {
+            Ciudadano ciudadano = db.Ciudadanos.Find(id);
+            if (ciudadano == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(ciudadano);
+        }
+
+        // PUT: api/Ciudadanoes/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCiudadano(int id, Ciudadano ciudadano)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != ciudadano.Id_Ciudadanos)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(ciudadano).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CiudadanoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Ciudadanoes
+        [ResponseType(typeof(Ciudadano))]
+        public IHttpActionResult PostCiudadano(Ciudadano ciudadano)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Ciudadanos.Add(ciudadano);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CiudadanoExists(ciudadano.Id_Ciudadanos))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = ciudadano.Id_Ciudadanos }, ciudadano);
+        }
+
+        // DELETE: api/Ciudadanoes/5
+        [ResponseType(typeof(Ciudadano))]
+        public IHttpActionResult DeleteCiudadano(int id)
+        {
+            Ciudadano ciudadano = db.Ciudadanos.Find(id);
+            if (ciudadano == null)
+            {
+                return NotFound();
+            }
+
+            db.Ciudadanos.Remove(ciudadano);
+            db.SaveChanges();
+
+            return Ok(ciudadano);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CiudadanoExists(int id)
+        {
+            return db.Ciudadanos.Count(e => e.Id_Ciudadanos == id) > 0;
+        }
+    }
+}
